843c7a0778fdc84489fafb5644a505c2cf1ff5b9
felipebuttinger <felipegarciabuttinger@gmail.com>
2025-06-03 12:13:27 -0300

    login-finish

---
+++ root/js/login.js
@@ -0,0 +1,24 @@
+async function login(){
+    const token = localStorage.getItem("token");
+    let name = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    const response = await fetch('http://localhost:3000/login', {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json' 
+                },
+                
+                body: JSON.stringify({ name, password })
+            });
+            if (response.ok) {
+                
+                const data = await response.json();
+                
+                localStorage.setItem('token', data.token); 
+                
+                window.location.href = 'home.html'; 
+            } else {
+              
+            }
+}
